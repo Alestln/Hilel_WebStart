acb597c5fe9adefd7ada61d9165a1757b9ef07dd
Alestln <itworkplace46@gmail.com>
2024-03-15 12:50:11 +0200

    initial commit

---
+++ Hilel_WebStart/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Hilel_WebStart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hilel_WebStart.Controllers;
+
+[ApiController]
+[Route($"{nameof(ProductController)}/")]
+public class ProductController : ControllerBase
+{
+    [HttpGet]
+    [Route($"{nameof(GetProductList)}")]
+    public Task<IActionResult> GetProductList()
+    {
+        var products = new List<Product>()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Apple",
+                Price = 25.3m
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Banana",
+                Price = 57.31m
+            },
+            new()
+            {
+                Id = 3,
+                Name = "Cocos",
+                Price = 120.5m
+            }
+        };
+
+        return Task.FromResult<IActionResult>(Ok(products));
+    }
+}
---
+++ Hilel_WebStart/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Hilel_WebStart.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
